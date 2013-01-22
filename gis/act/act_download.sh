ab8f910e9ac013419a36b9cf5e720f5390ae734a
dwurf <github@darrenwurf.com>
2013-01-22 10:52:40 -0500

    remove redundant code

--- gis/act/act_download.sh
+++ gis/act/act_download.sh
@@ -2,35 +2,10 @@
 
 sudo apt-get -y install spatialite-bin
 wget http://downloads.cloudmade.com/oceania/australia_and_new_zealand/australia/australian_capital_territory/australian_capital_territory.osm.bz2
-wget http://downloads.cloudmade.com/oceania/australia_and_new_zealand/australia/australian_capital_territory/australian_capital_territory.administrative.osm.bz2
 
 time bunzip2 australian_capital_territory.osm.bz2
-time bunzip2 australian_capital_territory.administrative.osm.bz2
 
 time spatialite_osm_raw -o australian_capital_territory.osm -d australian_capital_territory.sqlite
-time spatialite_osm_raw -o australian_capital_territory.administrative.osm -d australian_capital_territory.administrative.sqlite
-
-
-time spatialite australian_capital_territory.sqlite << EOF
-attach 'australian_capital_territory.administrative.sqlite' as admin;
-
-begin;
-insert or replace into osm_nodes select * from admin.osm_nodes;
-insert or replace into osm_ways select * from admin.osm_ways;
-insert or replace into osm_relations select * from admin.osm_relations;
-insert or replace into osm_node_tags select * from admin.osm_node_tags;
-insert or replace into osm_way_tags select * from admin.osm_way_tags;
-insert or replace into osm_relation_tags select * from admin.osm_relation_tags;
-insert or replace into osm_way_node_refs select * from admin.osm_way_node_refs;
-insert or replace into osm_relation_node_refs select * from admin.osm_relation_node_refs;
-insert or replace into osm_relation_relation_refs select * from admin.osm_relation_relation_refs;
-insert or replace into osm_relation_way_refs select * from admin.osm_relation_way_refs;
-commit;
-detach database admin;
-.quit
-
-EOF
-
 
 time spatialite australian_capital_territory.sqlite << EOF
 select AddGeometryColumn('osm_ways', 'Geometry', 4326, 'LINESTRING', 'XY');
