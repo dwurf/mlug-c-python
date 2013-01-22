111288b036eb74ab94571614296ec7e4056af9ca
dwurf <github@darrenwurf.com>
2013-01-22 16:28:36 -0500

    Update gis data setup script

--- gis/vic_download.sh
+++ gis/vic_download.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
+osm_url=http://downloads.cloudmade.com/oceania/australia_and_new_zealand/australia/victoria/victoria.osm.bz2
+osm_base=$(basename "$osm_url" .osm.bz2)
+
 sudo apt-get -y install spatialite-bin
-wget http://downloads.cloudmade.com/oceania/australia_and_new_zealand/australia/victoria/victoria.osm.bz2
+if [ -f $osm_base.osm -o -f ${osm_base}.osm.bz2 ]; then
+    true
+else
+    wget "${osm_url}"
+fi
 
-time bunzip2 victoria.osm.bz2
+if [ -f suburbs.zip -o -f ../suburbs.zip ]; then
+    true
+else
+    wget -O suburbs.zip 'http://www.abs.gov.au/AUSSTATS/subscriber.nsf/log?openagent&2923030001ssc06aaust.zip&2923.0.30.001&Data%20Cubes&2E96C5C5F3054EDFCA25731A002140DD&0&2006&17.07.2007&Latest'
+fi
 
-time spatialite_osm_raw -o victoria.osm -d victoria.sqlite
+if [ -f "${osm_base}".osm.bz2 -a ! -f "${osm_base}".osm ]; then
+    bunzip2 "${osm_base}".osm.bz2
+fi
 
-time spatialite victoria.sqlite << EOF
+if [ -f "${osm_base}".sqlite ]; then
+    rm "${osm_base}".sqlite
+fi
+
+spatialite_osm_raw -o "${osm_base}".osm -d "${osm_base}".sqlite
+
+spatialite "${osm_base}".sqlite << EOF
 select AddGeometryColumn('osm_ways', 'Geometry', 4326, 'LINESTRING', 'XY');
 select AddGeometryColumn('osm_relations', 'Geometry', 4326, 'GEOMETRYCOLLECTION', 'XY');
 update osm_ways set "Geometry" = (
